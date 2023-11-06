f7d5f6cc96ff140f07aa6e13df3b530d1293dab5
Marion.girard <marion.girard''@gmail.com>
2023-11-06 16:18:02 +0100

    m

--- main.js
+++ main.js
@@ -1,33 +1,54 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const firstName = 'marion';
-const lastName = 'girard';
 
 //const helloWorld = <h1> Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)} </span> <span className="red-text">{lastName.toUpperCase()}</span></h1>
 
 
 
+/*function formatFirstName(value){
+    return <span>{value[0].toUpperCase() + value.slice(1)}</span>
+}
 
 
-const formatFirstName = (value) => value[0].toUpperCase() + value.slice(1)
+
+function formatLastName(value){
+    return <span  className="red-text">{value.toUpperCase()}</span>
+}
 
 
-const formatLastName = (value) => value.toUpperCase() 
 
 
 const helloWorld =(
-<h1>
+    <h1>
     hello
-    <span> {formatFirstName(firstName)}</span>{" "}
-    <span className="red-text"> {formatLastName(lastName)}</span>
-</h1>
-)
-
-
-
-
-
-
+    {formatFirstName(firstName)}{" "}
+    {formatLastName(lastName)}
+    </h1>
+    )*/
+    
+    
+    class FormatFirstName extends React.Component {
+        render() {
+            return <span>{this.props.value[0].toUpperCase() + this.props.value.slice(1)}</span>;
+        }
+    }
+    
+    class FormatLastName extends React.Component {
+        render() {
+            return <span className="red-text">{this.props.value.toUpperCase()}</span>;
+        }
+    }
+    
+    const firstName = 'marion';
+    const lastName = 'girard';
+    
+    const HelloWorld = (
+        <h1>
+            Hello <FormatFirstName value={firstName} /> <FormatLastName value={lastName} />
+        </h1>
+    );
+    
+    ReactDOM.render(HelloWorld, document.getElementById('app'));
 // Solution sans bonus
 //const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
 
@@ -53,5 +74,5 @@ const helloWorld =(
 
  */
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+//ReactDOM.render(helloWorld, document.querySelector('#app'));
 
